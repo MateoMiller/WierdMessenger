44ff0ea40d6d5f1142a377f4acd27eea0787c501
yurii <yurii@DESKTOP-M79MGU7>
2024-05-21 03:00:28 +0500

    Заглушка под функциональные тесты

---
+++ FunctionalTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Autofac;
+using Messenger;
+
+namespace FunctionalTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
