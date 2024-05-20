6faed37cf712451def3dcecd37cd3909d5e25383
yurii <yurii@DESKTOP-M79MGU7>
2024-05-20 22:08:15 +0500

    Миграции

---
+++ Messenger/Migrations/20240519221417_Initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "authInfo",
+                columns: table => new
+                {
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "bytea", fixedLength: true, maxLength: 16, nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authInfo", x => x.Login);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cookies",
+                columns: table => new
+                {
+                    AuthSid = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cookies", x => x.AuthSid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImageBase64 = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_authInfo_UserId",
+                table: "authInfo",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "authInfo");
+
+            migrationBuilder.DropTable(
+                name: "cookies");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ Messenger/Migrations/20240519221521_InitialChat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Migrations
+{
+    public partial class InitialChat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chats",
+                columns: table => new
+                {
+                    ChatId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chats", x => x.ChatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.MessageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userschats",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userschats", x => new { x.UserId, x.ChatId });
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_ChatId",
+                table: "messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userschats_ChatId",
+                table: "userschats",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userschats_UserId",
+                table: "userschats",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chats");
+
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "userschats");
+        }
+    }
+}
