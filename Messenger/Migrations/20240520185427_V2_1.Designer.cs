53e83a24320578d4f8b5f3fb110612bcce1cedd9
yurii <yurii@DESKTOP-M79MGU7>
2024-05-21 02:56:15 +0500

    Новые миграции

--- Messenger/Migrations/20240520185427_V2_1.Designer.cs
+++ Messenger/Migrations/20240520185427_V2_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Messenger.DatabaseConnection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Messenger.Migrations
 {
     [DbContext(typeof(ChatContext))]
-    partial class ChatContextModelSnapshot : ModelSnapshot
+    [Migration("20240520185427_V2_1")]
+    partial class V2_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Messenger/Migrations/20240520185427_V2_1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Migrations
+{
+    public partial class V2_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MessageText",
+                table: "messages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SendDate",
+                table: "messages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SenderId",
+                table: "messages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Base64Image",
+                table: "chats",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "chats",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessageText",
+                table: "messages");
+
+            migrationBuilder.DropColumn(
+                name: "SendDate",
+                table: "messages");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "messages");
+
+            migrationBuilder.DropColumn(
+                name: "Base64Image",
+                table: "chats");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "chats");
+        }
+    }
+}
